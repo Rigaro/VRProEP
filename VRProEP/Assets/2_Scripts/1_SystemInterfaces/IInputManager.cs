16f276a3c0ed97edbe0e87f6a7bb9e42a32f3112
Ricardo Garcia <ricardog@student.unimelb.edu.au>
2018-10-10 18:21:51 +1100

    Created system interfaces.
    closes #1.

---
+++ VRProEP/Assets/2_Scripts/1_SystemInterfaces/IInputManager.cs
@@ -0,0 +1,23 @@
+﻿//======= Copyright (c) Melbourne Robotics Lab, All rights reserved. ===============
+
+/// <summary>
+/// Interface for managers that handle input from users and provide references to devices.
+/// Examples of input systems: sEMG interface, kinematic synergy interface.
+/// </summary>
+public interface IInputManager
+{
+    /// <summary>
+    /// Generates an updated reference for the given device channel.
+    /// Should only be called during Physics updates, Monobehaviour : FixedUpdate.
+    /// </summary>
+    /// <param name="channel">The channel number.</param>
+    /// <returns>The updated reference.</returns>
+    float GenerateReference(int channel);
+
+    /// <summary>
+    /// Generates the set of references needed to update all devices.
+    /// Should only be called during Physics updates, Monobehaviour : FixedUpdate.
+    /// </summary>
+    /// <returns>The updated set of references.</returns>
+    float[] GenerateReference();
+}
