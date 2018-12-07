9975d480df54de7dc50a24a6eded2c4534339836
Ricardo Garcia <ricardog@student.unimelb.edu.au>
2018-12-07 18:00:30 +1100

    Task timer
    Created task timer object and manager for timing the grasp and drop-off task. Closes #24.

---
+++ VRProEP/Assets/2_Scripts/3_ExperimentCore/2_ExperimentManagers/TaskTimer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskTimer : MonoBehaviour {
+
+    private bool enable = false;
+    private float timer = 0.0f;
+
+    public float Timer
+    {
+        get
+        {
+            return timer;
+        }
+    }
+
+    /// <summary>
+    /// Resets the timer to 0.
+    /// </summary>
+    public void ResetTimer () {
+        timer = 0.0f;
+    }
+	
+	// Deterministic update
+	void FixedUpdate () {
+        if (enable)
+            timer += Time.fixedDeltaTime;
+	}
+
+    /// <summary>
+    /// Resets and starts timer.
+    /// </summary>
+    public void StartTimer()
+    {
+        ResetTimer();
+        enable = true;
+    }
+
+    /// <summary>
+    /// Stops the timer and returns its value.
+    /// </summary>
+    /// <returns>The time.</returns>
+    public float StopTimer()
+    {
+        enable = false;
+        return timer;
+    }
+
+    public bool IsEnabled()
+    {
+        return enable;
+    }
+}
