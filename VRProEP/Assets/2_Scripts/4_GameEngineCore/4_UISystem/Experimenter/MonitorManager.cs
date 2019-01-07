08a4460583122b63ed6dfa740f4286bef6a58649
Ricardo Garcia <ricardog@student.unimelb.edu.au>
2019-01-07 17:43:35 +1100

    Added experiment UI system: HUD, Console, Monitor
    Added the experiment UI system consisting of HUD, Console and Monitor. Each has its own handle script, rather than centralized as in OSI.

---
+++ VRProEP/Assets/2_Scripts/4_GameEngineCore/4_UISystem/Experimenter/MonitorManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using VRProEP.GameEngineCore;
+
+public class MonitorManager : ConsoleManager
+{
+    public TextMeshPro generalArea;
+    public TextMeshPro errorArea;
+
+    public override void DisplayText(string text, float time)
+    {
+        StartCoroutine(DisplayTextCoroutine(generalArea, text, time));
+    }
+
+    public override void DisplayError(int errorCode, string text)
+    {
+        StartCoroutine(DisplayTextCoroutine(errorArea, "Error # " + errorCode + ": " + text, 3.0f));
+    }
+}
---
+++ VRProEP/Assets/2_Scripts/4_GameEngineCore/4_UISystem/User/HUDManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUDManager : MonoBehaviour
+{
+
+    public GameObject hudText;
+    public GameObject hudGraphics;
+    public bool minimize = false;
+    public bool maximize = false;
+
+    private TextMeshPro hudTextMesh;
+
+    private void Start()
+    {
+        // Get HUD TextMesh
+        hudTextMesh = hudText.GetComponent<TextMeshPro>();
+    }
+
+    private void Update()
+    {
+        // Animate HUD.
+        hudGraphics.transform.Rotate(new Vector3(0.0f, 0.0f, 10.0f)*Time.deltaTime);
+        
+        // Debug
+        if (minimize)
+        {
+            MinimizeHUD();
+            minimize = false;
+        }
+
+        if (maximize)
+        {
+            MaximizeHUD();
+            maximize = false;
+        }
+    }
+
+    public void MinimizeHUD()
+    {
+        hudText.SetActive(false);
+        hudGraphics.transform.localScale = new Vector3(0.5f, 0.5f);
+    }
+
+    public void MaximizeHUD()
+    {
+        hudText.SetActive(true);
+        hudGraphics.transform.localScale = new Vector3(1.0f, 1.0f);
+    }
+
+    public void DisplayText(string text, float time)
+    {
+        MaximizeHUD();
+        StartCoroutine(DisplayTextCoroutine(text, time));
+    }
+
+    private IEnumerator DisplayTextCoroutine(string text, float time)
+    {
+        hudTextMesh.text = text;
+        yield return new WaitForSecondsRealtime(time);
+        hudTextMesh.text = "";
+        MinimizeHUD();
+    }
+}
