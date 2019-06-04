cf55c36ce702208e2a034ffc23dc3e461286fe85
Ricardo Garcia <ricardog@student.unimelb.edu.au>
2019-06-04 15:15:07 +1000

    Added interfaces for performance evaluators
    Added interfaces for performance evaluators to AdaptationCore.

---
+++ VRProEP/Assets/2_Scripts/1_SystemInterfaces/ICostFunction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRProEP.AdaptationCore
+{
+    public interface ICostFunction
+    {
+        /// <summary>
+        /// Sets the cost function weights.
+        /// </summary>
+        /// <param name="W"> The weight vector. </param>
+        void SetWeights(float[] W);
+        /// <summary>
+        /// Calculates the cost given the cost/objective function.
+        /// </summary>
+        /// <param name="u"> The input vector. </param>
+        /// <returns> The updated cost. </returns>
+        float UpdateCost(float[] u);
+    }
+}
---
+++ VRProEP/Assets/2_Scripts/1_SystemInterfaces/IPerformanceManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRProEP.AdaptationCore
+{
+    public interface IPerformanceManager
+    {
+        /// <summary>
+        /// Adds the given measure of performance for data analysis.
+        /// </summary>
+        /// <param name="pe"> The performance evaluator to add. </param>
+        void AddPerformanceEvaluator(IPerformanceEvaluator pe);
+
+        /// <summary>
+        /// Removes the given measure of performance for data analysis.
+        /// </summary>
+        /// <param name="pe"> The performance evaluator to remove. </param>
+        void RemovePerformanceEvaluator(IPerformanceEvaluator pe);
+
+        /// <summary>
+        /// Sets the cost function to be used for cost calculation.
+        /// </summary>
+        /// <param name="cf"> The desired cost function. </param>
+        void SetCostFunction(ICostFunction cf);
+
+        /// <summary>
+        /// Updates the cost with the given data.
+        /// </summary>
+        /// <param name="dataBuffers"> The data-sets to be used to update the cost. </param>
+        /// <returns> The updated cost. </returns>
+        float UpdateCost(List<List<float>> dataBuffers);
+    }
+}
