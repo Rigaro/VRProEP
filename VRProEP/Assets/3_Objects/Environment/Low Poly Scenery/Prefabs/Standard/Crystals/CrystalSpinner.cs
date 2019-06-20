f4479ad52e77f23c81067372109a234ce1a6ff22
Ricardo Garcia <ricardog@student.unimelb.edu.au>
2019-06-20 17:48:43 +1000

    New world template: Fantasy
    Created a new world template with a Fantasy theme.

---
+++ VRProEP/Assets/3_Objects/Environment/Low Poly Scenery/Prefabs/Standard/Crystals/CrystalSpinner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSpinner : MonoBehaviour
+{
+    public float rotationSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, rotationSpeed * Time.deltaTime, 0.0f);
+    }
+}
