674f672c45f2936903bf8ec7962a7e4da16723f2
Ricardo Garcia <ricardog@student.unimelb.edu.au>
2019-06-27 13:39:42 +1000

    Added templates for feedback managers

---
+++ VRProEP/Assets/2_Scripts/2_ProsthesisCore/6_FeedbackManagers/BoniManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRProEP.ProsthesisCore
+{
+    public class BoniManager : BasicFeedbackManager
+    {
+        /// <summary>
+        /// Updates the feedback provided to the user for the given channel.
+        /// Should only be called during Physics updates, Monobehaviour : FixedUpdate.
+        /// </summary>
+        /// <param name="channel">The channel number.</param>
+        /// <param name="sensorData">The sensor data from the device.</param>
+        public override void UpdateFeedback(int channel, float[] sensorData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates the feedback provided to the user for at all channels.
+        /// Should only be called during Physics updates, Monobehaviour : FixedUpdate.
+        /// </summary>
+        /// <param name="sensorData">The sensor data from the device.</param>
+        public override void UpdateAllFeedback(float[] sensorData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
