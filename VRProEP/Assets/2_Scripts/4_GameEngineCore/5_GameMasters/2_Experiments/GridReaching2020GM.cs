e4f51aaec993bfb8fc1bf9a8f2a4f95fc92f95b9
tianshi-yu <54522764+tianshi-yu@users.noreply.github.com>
2021-07-30 15:15:58 +1000

    Test GridReaching for TH
    Enabled the collider in ACESHand_R

--- VRProEP/Assets/2_Scripts/4_GameEngineCore/5_GameMasters/2_Experiments/GridReaching2020GM.cs
+++ VRProEP/Assets/2_Scripts/4_GameEngineCore/5_GameMasters/2_Experiments/GridReaching2020GM.cs
@@ -180,22 +180,26 @@ public class GridReaching2020GM : GameMaster
             //
             // Debug using able-bodied configuration
             //
+            /*
             AvatarSystem.LoadPlayer(SaveSystem.ActiveUser.type, AvatarType.AbleBodied);
             AvatarSystem.LoadAvatar(SaveSystem.ActiveUser, AvatarType.AbleBodied);
+            */
 
             //
             // Debug prosthetic
             //
-            //AvatarSystem.LoadPlayer(UserType.Ablebodied, AvatarType.Transhumeral);
-            //AvatarSystem.LoadAvatar(SaveSystem.ActiveUser, AvatarType.Transhumeral);
-            //// Initialize prosthesis
-            //GameObject prosthesisManagerGO = GameObject.FindGameObjectWithTag("ProsthesisManager");
-            //ConfigurableElbowManager elbowManager = prosthesisManagerGO.AddComponent<ConfigurableElbowManager>();
-            //elbowManager.InitializeProsthesis(SaveSystem.ActiveUser.upperArmLength, (SaveSystem.ActiveUser.forearmLength + SaveSystem.ActiveUser.handLength / 2.0f));
-            //// Set the reference generator to jacobian-based.
-            ////elbowManager.ChangeReferenceGenerator("VAL_REFGEN_JACOBIANSYN");
-            //// Set the reference generator to linear synergy.
-            //elbowManager.ChangeReferenceGenerator("VAL_REFGEN_LINKINSYN");
+            
+            AvatarSystem.LoadPlayer(UserType.Ablebodied, AvatarType.Transhumeral);
+            AvatarSystem.LoadAvatar(SaveSystem.ActiveUser, AvatarType.Transhumeral);
+            // Initialize prosthesis
+            GameObject prosthesisManagerGO = GameObject.FindGameObjectWithTag("ProsthesisManager");
+            ConfigurableElbowManager elbowManager = prosthesisManagerGO.AddComponent<ConfigurableElbowManager>();
+            elbowManager.InitializeProsthesis(SaveSystem.ActiveUser.upperArmLength, (SaveSystem.ActiveUser.forearmLength + SaveSystem.ActiveUser.handLength / 2.0f));
+            // Set the reference generator to jacobian-based.
+            //elbowManager.ChangeReferenceGenerator("VAL_REFGEN_JACOBIANSYN");
+            // Set the reference generator to linear synergy.
+            elbowManager.ChangeReferenceGenerator("VAL_REFGEN_LINKINSYN");
+            
         }
 
     }
